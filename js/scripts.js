e12004ec573c49b8c1f68957bf3a549f0aae67b0
Glenn0206 <s10270340@connect.np.edu.sg>
2025-01-27 11:11:42 +0800

    Update signup page link to RestDB

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const APIKEY = '678e048fb0ed22eec94487fa'
+    const APIKEY = '6796f154f9d2bbc852181e24'
     //create submit form listener
     document.getElementById("signup-submit").addEventListener("click", function(e) {
         e.preventDefault();
 
         //get the form data
-        let userName = document.getElementById("login-username").value;
-        let password = document.getElementById("login-password").value;
-
+        let userName = document.getElementById("signup-username").value;
+        let phone = document.getElementById("signup-phone").value;
+        let email = document.getElementById("signup-email").value;
+        let password = document.getElementById("signup-password").value;
+        
         //get form values when submit button is clicked
         let jsondata = {
             "username": userName,
-            "password": password
+            "number": phone,
+            "email": email,
+            "password": password,
         };
     
-            //create our AJAX settings.
+        //create our AJAX settings.
         let settings = {
             method: 'POST',
             headers: {
@@ -24,14 +28,17 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             body: JSON.stringify(jsondata),
             beforeSend: function() {
+                alert("test");
                 //disable the submit button
                 document.getElementById("signup-submit").disabled = true;
                 //reset the form
-                document.getElementById("login-username").reset();
-                document.getElementById("login-password").reset();
+                document.getElementById("signup-username").reset();
+                document.getElementById("signup-phone").reset();
+                document.getElementById("signup-email").reset();
+                document.getElementById("signup-password").reset();
             }
         }
-        fetch("https://fedassg2-2419.restdb.io/rest/user-login-data", settings)
+        fetch("https://fedassg2-2419.restdb.io/rest/user-signup-data", settings)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
