6131f155b338bcfd07acd1f82cb9397fceb118db
Lee Wei Hang <167171171+radrium@users.noreply.github.com>
2025-01-27 10:53:25 +0800

    Testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const APIKEY = '678e048fb0ed22eec94487fa'
+    const APIKEY = '6796f154f9d2bbc852181e24'
     //create submit form listener
-    document.getElementById("signup-submit").addEventListener("click", function(e) {
+    document.getElementById("login-submit").addEventListener("click", function(e) {
         e.preventDefault();
 
         //get the form data
@@ -25,17 +25,17 @@ document.addEventListener('DOMContentLoaded', function() {
             body: JSON.stringify(jsondata),
             beforeSend: function() {
                 //disable the submit button
-                document.getElementById("signup-submit").disabled = true;
+                document.getElementById("login-submit").disabled = true;
                 //reset the form
                 document.getElementById("login-username").reset();
                 document.getElementById("login-password").reset();
             }
         }
-        fetch("https://fedassg2-2419.restdb.io/rest/user-login-data", settings)
+        fetch("https://fedassg2-2419.restdb.io/rest/user-signup-data", settings)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                document.getElementById("signup-submit").disabled = false;
+                document.getElementById("login-submit").disabled = false;
             });
         });
     });
