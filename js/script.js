c1831305c900d4b4644c5a95bf1be0d86470dbff
Glenn0206 <s10270340@connect.np.edu.sg>
2025-02-03 11:54:07 +0800

    Added code to check whether admin or user logged in

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('login-btn').style.display = 'none';
         document.getElementById('signup-btn').style.display = 'none';
         document.getElementById('icon-btn').style.display = 'block';
+        // Check if the logged-in user is an admin
+        if (loggedInUser.username === 'admin') {
+            document.querySelector('.admin-panel').style.display = 'block';
+            document.querySelector('.user-panel').style.display = 'none';
+        }
     } else {
         document.getElementById('login-btn').style.display = 'block';
         document.getElementById('signup-btn').style.display = 'block';
@@ -18,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('login-btn').style.display = 'block';
             document.getElementById('signup-btn').style.display = 'block';
             document.getElementById('icon-btn').style.display = 'none';
+            document.querySelector('.admin-panel').style.display = 'none';
         });
     }
 });
