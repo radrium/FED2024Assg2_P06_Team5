2db7d307b1cd6f7d42ac46cda0f8f3ad69084cbb
Glenn0206 <s10270340@connect.np.edu.sg>
2025-02-03 12:58:45 +0800

    Removed validation to check whether is user or admin

--- js/script.js
+++ js/script.js
@@ -4,17 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('login-btn').style.display = 'none';
         document.getElementById('signup-btn').style.display = 'none';
         document.getElementById('icon-btn').style.display = 'block';
-        // Check if the logged-in user is an admin
-        if (loggedInUser.username === 'admin') {
-            document.querySelector('.admin-panel').style.display = 'block';
-            document.querySelector('.user-panel').style.display = 'none';
-        }
+
     } else {
         document.getElementById('login-btn').style.display = 'block';
         document.getElementById('signup-btn').style.display = 'block';
         document.getElementById('icon-btn').style.display = 'none';
+        // document.querySelector('.user-panel').style.display = 'none';
     }
 
+    // alert('test');
     const signoutBtn = document.getElementById('signout-btn');
     if (signoutBtn) {
         signoutBtn.addEventListener('click', function() {
@@ -23,7 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('login-btn').style.display = 'block';
             document.getElementById('signup-btn').style.display = 'block';
             document.getElementById('icon-btn').style.display = 'none';
-            document.querySelector('.admin-panel').style.display = 'none';
         });
     }
 });
